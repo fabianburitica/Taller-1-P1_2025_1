146946d0ce9ce0d789e0b54e2ca75a8a197c2a50
andres <andresf.toroo@gmail.com>
2025-07-30 10:14:47 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home (request):
-    return render(request, 'home.html', {'name':'greg lim'})
+    return render(request, 'home.html', {'name':'Andres Toro'})
 def about(request):
     return render(request, 'home.html') 
