e07aaf9a075c2cfaa383c08be30e0b509cdc9b6d
Fabian <fabian.andresburiticacardozo@gmail.com>
2025-07-30 11:41:23 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
-def home(request):
-     return HttpResponse('<h1>Welcome to the Movie Reviews Home Page</h1>')
+
 def about(request):
-    return HttpResponse('<h1>About Movie Reviews</h1>')
+    return render(request,'About.html',)
+
 def home(request):
-    return render(request, 'home.html', {'name':'Fabian Buritica'})
+   
+   searchTerm = request.GET.get('searchMovie')
+   if searchTerm:
+       movies = Movie.objects.filter(title__icontains=searchTerm)
+   else:
+       movies = Movie.objects.all()
+   return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
